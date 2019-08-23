3ce6bf971d7ab4380617dd0764e0442eaad18f5c
Paolo Rotolo <rotolopao@gmail.com>
2019-08-23 15:26:10 +0200

    Checkpoint 3

--- app/src/main/java/com/github/corouteam/devfestlevante_2019/MainActivity.kt
+++ app/src/main/java/com/github/corouteam/devfestlevante_2019/MainActivity.kt
@@ -2,15 +2,9 @@ package com.github.corouteam.devfestlevante_2019
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.github.corouteam.devfestlevante_2019.models.ApiResponse
-import com.github.corouteam.devfestlevante_2019.models.Show
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,24 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getShowAsLiveData().observe(this, Observer {
+        val viewmodel = ViewModelProviders.of(this).get(ShowViewModel::class.java)
+
+        viewmodel.getShow().observe(this, Observer {
             titleTextView.text = it.name
         })
     }
 
-    fun getShowAsLiveData(): LiveData<Show> {
-        val liveData = MutableLiveData<Show>()
 
-        RetrofitClient.getWebservice().getShow("43467").enqueue(object: Callback<ApiResponse>{
-            override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
-                liveData.value = response.body()!!.tvShow
-            }
-
-            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
-
-            }
-        })
-
-        return liveData
-    }
 }
