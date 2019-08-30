63819ddbeec93504b9797976e2899ba328804968
Paolo Rotolo <rotolopao@gmail.com>
2019-08-30 16:09:16 +0200

    2

--- app/src/main/java/com/github/corouteam/devfestlevante_2019/MainActivity.kt
+++ app/src/main/java/com/github/corouteam/devfestlevante_2019/MainActivity.kt
@@ -2,7 +2,11 @@ package com.github.corouteam.devfestlevante_2019
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.github.corouteam.devfestlevante_2019.models.ApiResponse
+import com.github.corouteam.devfestlevante_2019.models.Show
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,39 +20,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO: Create data class for response
-        // Ex: https://www.episodate.com/api/show-details?q={SHOW_ID}
-        // Will respond with:
-        /*
-            {
-                "tvShow":{
-                    "id":43467,
-                    "name":"Lucifer",
-                    "permalink":"lucifer",
-                    "url":"https:\/\/www.episodate.com\/tv-show\/lucifer",
-                    "description":"The Devil has come to Los Angeles\u2026Based upon the characters created by Neil Gaiman, Sam Kieth and Mike Dringenberg for DC Entertainment's Vertigo imprint, Lucifer<\/b> is the story of the original fallen angel. Bored and unhappy as the Lord of Hell, Lucifer Morningstar has abandoned his throne and retired to L.A., where he owns Lux, an upscale nightclub",
-                    "start_date":"2016-01-25",
-                    "country":"US"
-                }
-            }
+        getData().observe(this, Observer {
+            titleTextView.text = it.name
+        })
 
-         */
+    }
 
-        // TODO: Get data from webservice
+    fun getData(): LiveData<Show> {
+        val liveData = MutableLiveData<Show>()
+
         RetrofitClient.getWebservice().getShow(SHOW_ID).enqueue(object: Callback<ApiResponse> {
             override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
 
             }
 
             override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
-                titleTextView.text = response.body()!!.tvShow.name
+                liveData.value = response.body()!!.tvShow
             }
 
         })
 
-
-
+        return liveData
     }
-
 
 }
